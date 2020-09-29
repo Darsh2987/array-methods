00cdc2ee2c7b30d270a894bed42c9ffee6d32906
Darsh <darsh2987@gmail.com>
2020-09-29 15:18:57 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/assets/js/app.js",
+  output: {
+    filename: "bundled-script.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundled-styles.css",
+    }),
+    new HtmlWebpackPlugin({
+      title: "index",
+      template: "src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "./dist",
+            },
+          },
+          "css-loader",
+        ],
+      },
+    ],
+  },
+};
